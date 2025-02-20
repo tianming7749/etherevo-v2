45c3a03632519ba2f0c03870ec8ed85de4eb4443
tianming7749 <stanleyyang7749@gmail.com>
2025-02-20 09:07:57 +0800

    这里替换成您的提交信息，例如：修复了 GeneratorPrompt 组件的bug

--- src/pages/UserInfo/LifeEnvironment/AdditionalDetailsInput.tsx
+++ src/pages/UserInfo/LifeEnvironment/AdditionalDetailsInput.tsx
@@ -11,10 +11,11 @@ const AdditionalDetailsInput: React.FC<AdditionalDetailsInputProps> = ({
 }) => {
   return (
     <div>
-      <label>补充细节：</label>
+      <label>其他：</label>
       <textarea
-        value={value}
-        onChange={(e) => onChange("additional_details", e.target.value)}
+        onChange={(e) => {
+          onChange(e.target.value); 
+        }}
         placeholder="如果你愿意，请简要描述你的压力源或健康问题..."
         rows={4}
         style={{ width: "100%", padding: "10px" }}
